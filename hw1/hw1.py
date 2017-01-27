0adb31809c051b4a491a3f873ff192833b633971
Krishna Thiyagarajan <krisht@cooper.edu>
2017-01-27 17:16:48 -0500

    Changed hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -1,53 +1,100 @@
-# Krishna Thiyagarajan
-# 
-
+#!/bin/python
 
 import tensorflow as tf
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
+import math
+import sys
 
 
-def psi(x, mu, sigma):
-	return np.exp(-((x-mu)/sigma)**2)
+#font = {'family' : 'Adobe Caslon Pro',
+#        'size'   : 10}
 
-mu_noise = 0; 
-sigma_noise = 0.1; 
-N = 50; # Sample size
-M = 5; #Hyper variable size
+#matplotlib.rc('font', **font)
+#
 
-#Create samples
-x_data = np.random.uniform(low=0.0, high=1.0, size=N);
-noise = np.random.normal(mu_noise, sigma_noise, N);
-y_data = np.sin(2*np.pi*x_data) + noise; 
-
-orig_x = np.arange(0.0,1.0, 0.01);
-orig_y = np.sin(2*np.pi*orig_x);  
-plt.plot(orig_x, orig_y, 'k', x_data, y_data, 'ro')
-plt.show(); 
+N = 50; # Number of samples
+M = 5;  # Hyper parameters
 
 
-W = tf.Variable(tf.random_uniform([M], -1.0, 1.0)); 
-mu = tf.Variable(tf.random_uniform([M], -1.0,1.0)); 
-sigma = tf.Variable(tf.random_uniform([M], 0.1, 1.0)); 
-b = tf.Variable(tf.zeros[1]); 
 
-yhat = W* psi(x, mu, sigma) + b
+def defVariable(shape, name):
+        var = tf.get_variable(name=name,
+                                   dtype=tf.float32,
+                                   shape=shape,
+                                   initializer=tf.random_normal_initializer(stddev=0.1)
+        )
+        tf.add_to_collection('modelVars', var)
+        tf.add_to_collection('l2', tf.reduce_sum(tf.pow(var,2)))
+        return var
+    
+class GaussianRDFModel():
+    def __init__(self, sess, data, iterations, learnRate, gamma):
+        self.sess = sess
+        self.data = data
+        self.iterations = iterations
+        self.learnRate = learnRate
+        self.gamma = gamma
+        self.buildModel()
 
-# W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-# b = tf.Variable(tf.zeros([1]))
-# y = W * x_data + b
+    def gaussian(self, x, mu, sigma): 
+    	return tf.exp(-(x-mu)**2/sigma**2);
+        
+    def buildModel(self):
+        self.x = tf.placeholder(tf.float32, shape=[])
+        self.y = tf.placeholder(tf.float32, shape=[])
+        
+        w = defVariable([1, M], 'w')
+        mu = defVariable([M,1], 'mu')
+        sigma = defVariable([M,1], 'sigma')
+        b = defVariable([], 'b')
+        phiArr = np.array([])
 
-loss = tf.reduce_mean(0.5 * tf.square(yhat - y_data)); 
-optimizer = tf.train.GradientDescentOptimizer(0.1); 
-train = optimizer.minimize(loss); 
+        for k in range(M):
+        	phiArr = np.append(phiArr, self.gaussian(self.x, mu[k], sigma[k]));
+        phi = tf.stack(phiArr.tolist()); 
 
-init = tf.global_variables_initializer(); 
-sess = tf.Session(); 
-sess.run(init); 
+        self.yhat = b + tf.matmul(w, phi); 
 
-for step in range(10001):
-	sess.run(train);
-	if step % 100 == 0:
-		print step, sess.run(W), sess.run(mu), sess.run(sigma), sess.run(b); 
+        self.mse = tf.reduce_mean(tf.square(self.yhat - self.y))
+        self.l2_penalty = tf.reduce_sum(tf.get_collection('l2'))
+        self.loss = self.mse + self.gamma*self.l2_penalty
+        
+    def initTrainer(self):
+        modelVars = tf.get_collection('modelVars')            
+        self.optim = (tf.train.GradientDescentOptimizer(learning_rate=0.1).minimize(self.loss, var_list=modelVars))
+        self.sess.run(tf.global_variables_initializer())
 
+    def iterateTrainer(self, step, x, y):
+        loss, _ = self.sess.run([self.loss,self.optim],
+                                          feed_dict={self.x : x, self.y : y})
+        if step % 5 == 0: 
+        	print('Step: {} \t Loss: {}'.format(step, loss))
 
+        
+    def train(self):
+        for step in range(self.iterations):
+            for x, y in self.data():
+                self.iterateTrainer(step, x, y)
+
+    def infer(self, x):
+        return self.sess.run(self.yhat, feed_dict={self.x : x})
+
+def data():
+    sigmaNoise = 0.5
+    for _ in range(N):
+        x = np.random.uniform()
+        y  = np.sin(2 * np.pi * x) + np.random.normal(loc = 0, scale = sigmaNoise)
+        yield x, y
+
+sess = tf.Session()
+model = GaussianRDFModel(sess, data, iterations=101, learnRate=1e-2, gamma=1e-4)
+model.initTrainer()
+model.train()
+    
+with tf.variable_scope("", reuse = True):
+	print("W =", sess.run(tf.get_variable("w"))); 
+	print("Mu =", sess.run(tf.transpose(tf.get_variable("mu"))))
+	print("Sigma =", sess.run(tf.transpose(tf.get_variable("sigma"))))
+	print("b =", sess.run(tf.get_variable("b")))
